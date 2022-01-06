78f58c0c4ff92d18a623e6551858194cd5f429a3
Amirrr1987 <Maghami.A1987@gmail.com>
2022-01-06 16:46:29 +0330

    feat: WindiCSS
    - install WindiCSS
    - enable attributify config
    - import WindiCSS in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,7 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
+import "virtual:windi.css";
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+app.mount("#app");
