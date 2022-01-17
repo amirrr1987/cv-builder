8b6028c7a1941e73a7282c8e7cacfba417e6ed85
Amirrr1987 <Maghami.A1987@gmail.com>
2022-01-18 02:24:11 +0330

    feat: userInfo
    - create pdf layout width grid
    - import userInfo data from store width GetDataStore

--- src/pages/user/TheResume.vue
+++ src/pages/user/TheResume.vue
@@ -1,18 +1,41 @@
 <template>
-  <div class="the-resume">TheResume</div>
+  <div class="grid grid-cols-[16rem,1fr] gap-5">
+    <div class="bg-blue-100 p-3">
+      <div class="">{{ userInfo.fName }}</div>
+      <div class="">{{ userInfo.lName }}</div>
+      <div class="">{{ userInfo.jobTitle }}</div>
+      <div class="">{{ userInfo.aboutMe }}</div>
+      <div class="">{{ userInfo.email }}</div>
+      <div class="">{{ userInfo.mobile }}</div>
+      <div class="">{{ userInfo.aboutMe }}</div>
+    </div>
+
+    <div class="p-3">
+      <div class="">
+        software knowledge
+      </div>
+      <div class="">experience</div>
+
+    </div>
+  </div>
 </template>
 <script lang="ts">
-import { defineComponent } from "vue";
-
+import { computed, defineComponent } from "vue";
+import { GetDataStore } from "./../../store";
 export default defineComponent({
   name: "TheResume",
   setup() {
-    return {};
+    const userInfo = computed(() => {
+      return GetDataStore("UserInfo");
+    });
+    return {
+      userInfo,
+    };
   },
 });
 </script>
 <style>
-.the-resume{
-    @apply p-3 border rounded
+.the-resume {
+  @apply p-3 border rounded;
 }
 </style>
