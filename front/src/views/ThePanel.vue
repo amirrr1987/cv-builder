8c5c800621a7a58f0ac06c5fe4891dee607740f3
amirrr1987 <maghami.a1987@gmail.com>
2022-08-29 21:50:47 +0430

    feat: use antdv layout

--- front/src/views/ThePanel.vue
+++ front/src/views/ThePanel.vue
@@ -1,10 +1,75 @@
+
 <template>
-    <div class="grid grid-cols-12">
-        <TheSide />
-        <TheMain />
-    </div>
+    <Layout>
+        <LayoutHeader class="header">
+            <div class="logo" />
+            <Menu v-model:selectedKeys="selectedKeys1" theme="dark" mode="horizontal" :style="{ lineHeight: '64px' }">
+                <MenuItem key="1">nav 1</MenuItem>
+                <MenuItem key="2">nav 2</MenuItem>
+                <MenuItem key="3">nav 3</MenuItem>
+            </Menu>
+        </LayoutHeader>
+        <Layout>
+            <LayoutSider width="350" style="background: #fff">
+
+                <TheSide/>
+                 <Menu v-model:selectedKeys="selectedKeys2" v-model:openKeys="openKeys" mode="inline"
+                    :style="{ height: '100%', borderRight: 0 }">
+                    <SubMenu key="sub1">
+                        <template #title>
+                            <span>
+                                <user-outlined />
+                                subnav 1
+                            </span>
+                        </template>
+                        <MenuItem key="1">option1</MenuItem>
+                        <MenuItem key="2">option2</MenuItem>
+                        <MenuItem key="3">option3</MenuItem>
+                        <MenuItem key="4">option4</MenuItem>
+                    </SubMenu>
+                    <SubMenu key="sub2">
+                        <template #title>
+                            <span>
+                                <laptop-outlined />
+                                subnav 2
+                            </span>
+                        </template>
+                        <MenuItem key="5">option5</MenuItem>
+                        <MenuItem key="6">option6</MenuItem>
+                        <MenuItem key="7">option7</MenuItem>
+                        <MenuItem key="8">option8</MenuItem>
+                    </SubMenu>
+                    <SubMenu key="sub3">
+                        <template #title>
+                            <span>
+                                <notification-outlined />
+                                subnav 3
+                            </span>
+                        </template>
+                        <MenuItem key="9">option9</MenuItem>
+                        <MenuItem key="10">option10</MenuItem>
+                        <MenuItem key="11">option11</MenuItem>
+                        <MenuItem key="12">option12</MenuItem>
+                    </SubMenu>
+                    </Menu>
+            </LayoutSider>
+            <Layout style="padding: 0 24px 24px">
+                <Breadcrumb style="margin: 16px 0">
+                    <BreadcrumbItem>Home</BreadcrumbItem>
+                    <BreadcrumbItem>List</BreadcrumbItem>
+                    <BreadcrumbItem>App</BreadcrumbItem>
+                </Breadcrumb>
+                <LayoutContent :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }">
+                    <TheMain />
+                </LayoutContent>
+            </Layout>
+        </Layout>
+    </Layout>
 </template>
+
 <script setup lang="ts">
+
+import { Layout, LayoutHeader, LayoutSider, LayoutContent, Menu, MenuItem, SubMenu, Breadcrumb, BreadcrumbItem } from "ant-design-vue";
 import { onMounted } from "vue";
 import { useRoute } from "vue-router";
 import stores from '@/stores'
