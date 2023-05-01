c26a3f970a1ff056888a3f89eeaf3ed405abf177
Amir Maghami <Maghami.A1987@gmail.com>
2023-05-02 01:21:59 +0430

    style: change folder name

---
+++ client/src/views/TheLanding.vue
@@ -0,0 +1,11 @@
+<template>
+    <TheLandingHeader />
+    <TheLandingMain />
+    <TheLandingFooter />
+</template>
+<script setup lang="ts">
+import TheLandingHeader from '@/layouts/landing/TheLandingHeader.vue';
+import TheLandingMain from '@/layouts/landing/TheLandingMain.vue';
+import TheLandingFooter from '@/layouts/landing/TheLandingFooter.vue';
+
+</script>
